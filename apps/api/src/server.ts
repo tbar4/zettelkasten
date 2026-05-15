2173f1d5ef9b4589c89eba751b6a01c8f53ecb23
Trevor Barnes <trevor.barnes91@gmail.com>
2026-05-15 15:27:26 -0700

    feat(api): inbox endpoint with due/fleeting/highlights panes

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -8,6 +8,7 @@ import { linksRoute, noteLinksRoute } from "./routes/links";
 import { tagsRoute, noteTagsRoute } from "./routes/tags";
 import { graphRoute } from "./routes/graph";
 import { reviewRoute } from "./routes/review";
+import { inboxRoute } from "./routes/inbox";
 
 export const app = new Hono();
 
@@ -22,6 +23,7 @@ app.route("/api/tags", tagsRoute);
 app.route("/api/notes", noteTagsRoute);
 app.route("/api/notes", reviewRoute);
 app.route("/api/graph", graphRoute);
+app.route("/api/inbox", inboxRoute);
 
 app.onError((err, c) => {
   if (err instanceof HTTPException) {
