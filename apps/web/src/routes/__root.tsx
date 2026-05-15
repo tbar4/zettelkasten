a9f4ecff790d2c4dd4d02e77d2dc11df08f70c4d
Trevor Barnes <trevor.barnes91@gmail.com>
2026-05-15 14:51:30 -0700

    feat(web): ⌘K command palette over FTS search

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,16 +1,25 @@
 import { Outlet, createRootRoute, Link } from "@tanstack/react-router";
+import { CommandPalette } from "../components/CommandPalette";
+import { useCommandPalette } from "../lib/use-command-palette";
 
-export const Route = createRootRoute({
-  component: () => (
+function Root() {
+  const { open, setOpen } = useCommandPalette();
+  return (
     <>
       <header style={{ marginBottom: 24 }}>
         <h1 style={{ margin: 0 }}>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
             Zettelkasten
           </Link>
+          <span style={{ marginLeft: 8, fontSize: 12, color: "#666" }}>
+            ⌘K to search
+          </span>
         </h1>
       </header>
       <Outlet />
+      <CommandPalette open={open} onClose={() => setOpen(false)} />
     </>
-  )
-});
+  );
+}
+
+export const Route = createRootRoute({ component: Root });
